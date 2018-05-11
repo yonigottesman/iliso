c3be02587cd7abfc73f658734a6a58254da829b3
Yonatan Gottesman <yonigo10@gmail.com>
2018-05-11 12:59:49 +0300

    update gitignire. fix example

--- sensore_example.py
+++ sensore_example.py
@@ -3,7 +3,7 @@ import json
 import time
 import requests
 
-ILISO_HOST = "127.0.0.0.1:1111"
+ILISO_HOST = "http://127.0.0.1:1111/update"
 
 
 payload = {"all_feeds":[{"feed_name":"motion","samples":[{"value":123,"time":time.time()}]}]}
