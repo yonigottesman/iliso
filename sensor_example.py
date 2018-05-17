9785c4c41d771fc6f3c425b6889690cf49e0b2b2
Yonatan Gottesman <yonigo10@gmail.com>
2018-05-17 23:59:31 +0300

    fix example

--- sensor_example.py
+++ sensor_example.py
@@ -3,8 +3,9 @@ import json
 import time
 import requests
 
-ILISO_HOST = "http://127.0.0.1:1111/update"
+ILISO_HOST = "https://iliso.herokuapp.com/update"
 
 
 payload = {"all_feeds":[{"feed_name":"motion","samples":[{"value":123,"time":time.time()}]}]}
-r = requests.post("http://127.0.0.1:1111/update", json=payload)
+r = requests.post(ILISO_HOST, json=payload)
+print(r)
